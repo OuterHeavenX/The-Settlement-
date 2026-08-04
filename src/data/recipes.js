bb3dc2c1418cba55e678258a122fc8aae8e45f09
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-03 22:09:38 -0500

    Add quarry and masonry production chain

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,4 +1,5 @@
 Settlement.RecipeDefs={
  millFlour:{id:"millFlour",name:"Mill Flour",duration:18,input:{wheat:3},output:{flour:2},xp:3},
- bakeBread:{id:"bakeBread",name:"Bake Bread",duration:24,input:{flour:2},output:{bread:3,food:6},xp:4}
+ bakeBread:{id:"bakeBread",name:"Bake Bread",duration:24,input:{flour:2},output:{bread:3,food:6},xp:4},
+ cutStone:{id:"cutStone",name:"Dress Stone",duration:22,input:{stone:4},output:{cutStone:2},xp:4}
 };
