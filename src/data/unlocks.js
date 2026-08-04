563687fcc14759278fb0f7de6b8947a15aac0455
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-03 22:10:05 -0500

    Add quarry and masonry town unlocks

--- src/data/unlocks.js
+++ src/data/unlocks.js
@@ -1,7 +1,7 @@
 Settlement.UnlockDefs={
- 2:["📦 Warehouse","🛤️ Dirt Path"],
+ 2:["📦 Warehouse","🛤️ Dirt Path","⛏️ Stone Quarry"],
  3:["🏠 Cottage Level 2","⚙️ Mill","🏹 Archery Tower"],
- 4:["🥖 Bakery"],
+ 4:["🥖 Bakery","🧱 Mason's Yard"],
  5:["🗺️ Second Frontier"],
  6:["🏘️ Cottage Level 3"]
 };
