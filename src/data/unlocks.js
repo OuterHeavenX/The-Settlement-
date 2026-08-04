8c7af6c918f249bcec24cf06cd8a1e55e50ea068
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-03 21:51:24 -0500

    Add files via upload

---
+++ src/data/unlocks.js
@@ -0,0 +1,7 @@
+Settlement.UnlockDefs={
+ 2:["📦 Warehouse","🛤️ Dirt Path"],
+ 3:["🏠 Cottage Level 2","⚙️ Mill","🏹 Archery Tower"],
+ 4:["🥖 Bakery"],
+ 5:["🗺️ Second Frontier"],
+ 6:["🏘️ Cottage Level 3"]
+};
