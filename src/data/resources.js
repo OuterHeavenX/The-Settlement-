50df8c03c469043cd9b943906e6d06ed0a263cdd
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-05 18:01:03 -0500

    Add iron industry resources

--- src/data/resources.js
+++ src/data/resources.js
@@ -1,6 +1,6 @@
 Settlement.ResourceDefs={
  gold:{name:"Gold",icon:"🪙"},wood:{name:"Wood",icon:"🪵"},stone:{name:"Stone",icon:"🪨"},food:{name:"Food",icon:"🍲"},
  wheat:{name:"Wheat",icon:"🌾"},flour:{name:"Flour",icon:"🥣"},bread:{name:"Bread",icon:"🍞"},
- cutStone:{name:"Cut Stone",icon:"🧱"},
+ cutStone:{name:"Cut Stone",icon:"🧱"},ironOre:{name:"Iron Ore",icon:"⛏️"},ironBar:{name:"Iron Bars",icon:"🔩"},tools:{name:"Tools",icon:"🛠️"},
  carrots:{name:"Carrots",icon:"🥕"},potatoes:{name:"Potatoes",icon:"🥔"},cabbage:{name:"Cabbage",icon:"🥬"},flax:{name:"Flax",icon:"🪻"},clay:{name:"Clay",icon:"🟤"}
 };
