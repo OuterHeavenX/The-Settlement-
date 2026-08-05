55706f921c07c7d63535c7d4ea01cd559140899b
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-05 18:01:37 -0500

    Add iron processing recipes

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,5 +1,7 @@
 Settlement.RecipeDefs={
  millFlour:{id:"millFlour",name:"Mill Flour",duration:18,input:{wheat:3},output:{flour:2},xp:3},
  bakeBread:{id:"bakeBread",name:"Bake Bread",duration:24,input:{flour:2},output:{bread:3,food:6},xp:4},
- cutStone:{id:"cutStone",name:"Dress Stone",duration:22,input:{stone:4},output:{cutStone:2},xp:4}
+ cutStone:{id:"cutStone",name:"Dress Stone",duration:22,input:{stone:4},output:{cutStone:2},xp:4},
+ smeltIron:{id:"smeltIron",name:"Smelt Iron",duration:30,input:{ironOre:3},output:{ironBar:1},xp:5},
+ forgeTools:{id:"forgeTools",name:"Forge Tools",duration:36,input:{ironBar:2},output:{tools:1},xp:7}
 };
