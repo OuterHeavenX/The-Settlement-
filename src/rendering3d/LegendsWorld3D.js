ede73c5dcc4381a070e8abb3fe2454373b5765cf
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-10 13:24:28 -0500

    feat: add bespoke fortified Hall of Legends 3D model

---
+++ src/rendering3d/LegendsWorld3D.js
@@ -0,0 +1,21 @@
+import * as THREE from "../../vendor/three/three.module.min.js";
+import {PermanentDayWorld3D} from "./PermanentDayWorld3D.js?v=0.14.0-legends";
+const T=64;
+export class LegendsWorld3D extends PermanentDayWorld3D{
+ buildStructure(b){if(b?.complete&&b.type==="hallOfLegends")return this.makeHallOfLegends(b);return super.buildStructure(b)}
+ makeHallOfLegends(b){
+  const g=new THREE.Group(),stone=this.mat("legendStone",0x65676b),stone2=this.mat("legendStone2",0x85858a),dark=this.mat("legendDark",0x272a31),roof=this.mat("legendRoof",0x303747),iron=this.mat("legendIron",0x25292e,{metalness:.45}),gold=this.mat("legendGold",0xb48b45,{metalness:.35}),cloth=this.mat("legendCloth",0x742f45),warm=this.mat("legendWarm",0xe7b465,{emissive:0xd98942,ei:.5});
+  const box=(w,h,d,m,x=0,y=0,z=0)=>this.part(g,this.box(w,h,d),m,x,y,z),cyl=(a,c,h,s,m,x=0,y=0,z=0)=>this.part(g,this.cyl(a,c,h,s),m,x,y,z);
+  box(292,18,292,stone,0,9,0);box(254,82,224,stone,0,58,5);box(226,8,198,stone2,0,101,5);
+  const mainRoof=this.part(g,this.cyl(0,174,72,4),roof,0,143,5);mainRoof.rotation.y=Math.PI/4;
+  for(const[x,z]of[[-126,-96],[126,-96],[-126,96],[126,96]]){cyl(28,32,104,10,stone,x,62,z);const r=this.part(g,this.cyl(0,43,58,8),roof,x,143,z);r.rotation.y=Math.PI/8;box(8,28,8,iron,x,183,z);this.part(g,this.sph(6,7,5),gold,x,200,z)}
+  box(58,74,18,dark,0,49,-116);box(48,62,8,iron,0,46,-126);box(82,8,28,stone2,0,9,-146);box(104,7,36,stone2,0,4,-154);
+  for(const x of[-82,-42,42,82])box(18,26,5,warm,x,62,-109);
+  for(const x of[-72,72]){box(6,78,6,iron,x,106,-126);box(42,58,3,cloth,x+(x<0?-18:18),119,-126);box(6,8,6,gold,x,148,-126)}
+  // Twin archer galleries make the building's defensive purpose readable.
+  for(const x of[-92,92]){box(54,18,30,dark,x,116,-114);for(const sx of[-14,0,14])box(7,16,7,stone2,x+sx,131,-114);box(38,4,5,gold,x,121,-133)}
+  // Trophy plinths along the front courtyard.
+  for(let i=0;i<5;i++){let x=-80+i*40;box(20,12,20,stone2,x,15,-126);box(4,20,4,gold,x,31,-126)}
+  g.position.set((b.x+b.w/2)*T,0,(b.y+b.h/2)*T);g.userData.buildingId=b.id;g.userData.legendHall=true;return g
+ }
+}
